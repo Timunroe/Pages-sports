787c1586cf388c33072a6b4c84b233a4f2763f99
Tim Munroe <munroe.tim@gmail.com>
2018-07-04 17:50:11 -0400

    July 4 mid

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, request, url_for, redirect
+import model
+import fetch
+import data
 
 app = Flask(__name__)
 
@@ -7,6 +10,18 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/feed', methods=['GET', 'POST'])
+def feed():
+    if request.method == 'POST':
+        if request.form['action'] == 'save':
+            model.parse_form(request.form)
+        if request.form['action'] == 'fetch':
+            model.get_new_data('feed')
+    template_data = {"items": model.get_lineup('feed')}
+    return render_template('feed.html', data=template_data, draft_check=False)
+
 
 @app.route('/about')
 def about():
---
+++ data.py
@@ -0,0 +1,49 @@
+from jinja2 import Environment, FileSystemLoader
+from string import Template
+import htmlmin
+import io
+import script_templates as tmpl
+import fetch
+import model
+import config as cfg
+
+# Put <script src="https://picabot.s3.amazonaws.com/pagejs/ticats_spec.js"></script> in DNN footer
+# put <div class="pica-results"></div> in DNN body
+
+
+def minify_html(s_html):
+    # returns string of minified html
+    return htmlmin.minify(s_html, remove_comments=True, remove_empty_space=True)
+
+
+def save_file_overwrite(s_contents, s_name):
+    print("Now in save file module")
+    build_directory = 'build'
+    with io.open(f"{build_directory}/{s_name}", "w+", encoding='utf8') as file:
+        file.write(s_contents)
+    print(f"File saved in {build_directory}: {s_name}")
+    return
+
+
+def build_template(file_name):
+    # NEED TO CREATE BUILD DIRECTORY IF IT DOESN'T EXIST!!!
+    print("Building template for DNN")
+    template_data = {"posts": model.get_lineup(file_name)}
+    template_name = file_name + ".html"
+    j2_env = Environment(loader=FileSystemLoader('templates'), trim_blocks=True)
+    html = j2_env.get_template(template_name).render(data=template_data)
+    html_minified = minify_html(html)
+    css = fetch.fetch_css()
+    script_template = getattr(x, 'foobar')
+    script_name = file_name + ".js"
+    script = Template(tmpl.script_template).substitute(css=css, minified=html_minified)
+
+    save_file_overwrite(script, script_name)
+    pass
+
+
+# if __name__ == "__main__":
+    # call api, update database
+    # model.get_new_data()
+    # build template
+    # build_template()
